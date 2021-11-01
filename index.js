986a39576357832e200b962b1a50f02b13606d74
Joe Sembler <joeysembler@gmail.com>
2021-10-31 20:42:21 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+// Joe Sembler
+// Flatiron School
+// Phase 0: "JavaScript Events Acting On Events Lab"
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 10}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+  });
